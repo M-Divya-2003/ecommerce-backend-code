74be8c09fd87b6e2fb0ca2214d5b26db7a9cf40a
Divya <divya28112003@gmail.com>
2025-08-07 10:35:48 +0530

    Initial backend only

--- server.js
+++ server.js
@@ -36,5 +36,7 @@ app.use('/api/cart', require('./routes/cart'));
 app.use('/api/wishlist', require('./routes/wishlist'));
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT=5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+})
